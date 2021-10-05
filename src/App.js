42b712f8d34d09816521c528cf46dde0f3fd2ef8
zaraki <ziiedkouki@gmail.com>
2021-10-05 15:02:32 +0200

    oneShot release

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import data from "./data";
+import List from "./components/List";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [people, setPepole] = useState(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <main>
+      <section className="container">
+        <h3>{people.length} Birthdays For Today</h3>
+        <List people={people} />
+        <button
+          onClick={() => {
+            setPepole([]);
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Clear All{" "}
+        </button>
+      </section>
+    </main>
   );
-}
+};
 
 export default App;
